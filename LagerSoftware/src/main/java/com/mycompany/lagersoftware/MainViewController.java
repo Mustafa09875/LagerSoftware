2b7049d2465a45dca96188deff2091959a83bc4f
lukas-es <69432736+lukas-es@users.noreply.github.com>
2021-08-18 00:17:41 +0200

    remove

--- LagerSoftware/src/main/java/com/mycompany/lagersoftware/MainViewController.java
+++ LagerSoftware/src/main/java/com/mycompany/lagersoftware/MainViewController.java
@@ -92,8 +92,6 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void BtnVermietung(ActionEvent event) throws IOException {
-        AnchorPane  pane = FXMLLoader.load(getClass().getResource("VermietungView.fxml"));
-        borderpane.setCenter(pane);
     }
 
     @FXML
