71a58b977a54b059bab3b38247bf6f80727dc3d9
Mustafa09875 <mustafa098@outlook.de>
2021-08-18 10:41:57 +0200

    neu

--- LagerSoftware/src/main/java/com/mycompany/lagersoftware/LagerVerwaltungViewController.java
+++ LagerSoftware/src/main/java/com/mycompany/lagersoftware/LagerVerwaltungViewController.java
@@ -210,28 +210,20 @@ public class LagerVerwaltungViewController implements Initializable {
     }
 
 
-    private void clickSelect(MouseEvent event) throws IOException { 
-         
-
-       /** int index = LWTechnikAnpassen.getSelectionModel().getSelectedIndex();
-       
-        App.setSelectedTechnik(getTechnik().get(index));
-        System.out.println(App.getSelectedTechnik().getName());**/
-        
-        
-        
-        
-        
-    }
 
     @FXML
     private void LWTechnikAnspassenSelect(MouseEvent event) throws IOException {
          TabPane.setVisible(false);
-
         AnchorPaneTechnikAnpassen.setVisible(false);
          
         AnchorPane  pane = FXMLLoader.load(getClass().getResource("TechnikAktualisierenView.fxml"));      
                 BorderPaneTechnikSelect.setCenter(pane);
+                
+                /** int index = LWTechnikAnpassen.getSelectionModel().getSelectedIndex();
+       
+        App.setSelectedTechnik(getTechnik().get(index));
+        System.out.println(App.getSelectedTechnik().getName());**/
+                
     }
 
 
