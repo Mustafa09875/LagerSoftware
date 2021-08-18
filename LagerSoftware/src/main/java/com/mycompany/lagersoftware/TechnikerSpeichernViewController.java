d89b41772316d5aa2d3894c704794e4a9078bff0
ApiSinghru <ap.singhru@gmail.com>
2021-08-18 17:29:39 +0200

    a

--- LagerSoftware/src/main/java/com/mycompany/lagersoftware/TechnikerSpeichernViewController.java
+++ LagerSoftware/src/main/java/com/mycompany/lagersoftware/TechnikerSpeichernViewController.java
@@ -7,7 +7,14 @@ package com.mycompany.lagersoftware;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -16,6 +23,21 @@ import javafx.fxml.Initializable;
  */
 public class TechnikerSpeichernViewController implements Initializable {
 
+    @FXML
+    private TabPane TabPane;
+    @FXML
+    private TextField TxtFTechnikerVorname;
+    @FXML
+    private TextField TxtFTechnikerNachname;
+    @FXML
+    private TextField TxtFTechnikerStundensatz;
+    @FXML
+    private TextField TxtFTechnikerBeschreibung;
+    @FXML
+    private AnchorPane BtnTechnikAnpassen;
+    @FXML
+    private ListView<?> LWTechnikAnpassen;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +45,21 @@ public class TechnikerSpeichernViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void BtnTechnikerSpeichern(ActionEvent event) {
+    }
+
+    @FXML
+    private void LWTechnikerSelect(MouseEvent event) {
+    }
+
+    @FXML
+    private void BtnLWTechnikerBearbeiten(ActionEvent event) {
+    }
+
+    @FXML
+    private void BtnLwTechnikerLöschen(ActionEvent event) {
+    }
     
 }
